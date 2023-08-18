06fad11dee80a5312c1c129d2374d0d902f2f3c2
chipfelzappe <chipfelzappe@hotmail.com>
2023-08-18 11:40:30 +0200

    server

--- server/sbg_server.py
+++ server/sbg_server.py
@@ -1,14 +1,20 @@
 import socket
 import threading
+import struct
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 BUFF_SIZE = 65536
+format_string = 'HBBBBBIdddd'
 sensor_data = ""
 
 def receive_sensor_data():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF,BUFF_SIZE)
+    matplotlib.use('WebAgg')
 
-    ip ="localhost"
+    ip ="0.0.0.0"
     port = 1235
 
     #binding ip and port number
@@ -22,10 +28,21 @@ def receive_sensor_data():
 # receives packet from sender and send them back to server for display
 def recv(s):
     while True:
-        packet,_ = s.recvfrom(BUFF_SIZE)
         global sensor_data
-        sensor_data = (packet.decode('utf-8'))
-        print(sensor_data)
+        packet,_ = s.recvfrom(struct.calcsize(format_string))
+        unpacked_data = struct.unpack(format_string, packet)
+        year, month, day, hour, minute, second, nanosecond, euler, altitude, latitude, longitude = unpacked_data
+    
+        # Create a plot
+        plt.figure(figsize=(10, 6))
+        plt.plot([euler[0], euler[1], euler[2]], marker='o')
+        plt.title("Euler Angles")
+        plt.xlabel("Axis")
+        plt.ylabel("Angle (degrees)")
+        plt.xticks(range(3), ['X', 'Y', 'Z'])
+    
+        # Display the plot
+        plt.show()
 
 if __name__ == "__main__":
    receive_sensor_data()
